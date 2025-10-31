5bfca4da762b37595f8728bb921c771cfa54a9ef
Sri.Sila <ssilasag@asu.edu>
2025-10-31 01:36:54 -0700

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
     / ___/ (_) | | | | | ||  __/ |    / ___/ (_) \ V  V /  __/ |   
     \/    \___/|_|_| |_|\__\___|_|    \/    \___/ \_/\_/ \___|_|   
 
-    Read the comments line-by-line and debug where it says CODE, the code will not run correctly in its current state
+    Read the comments line-by-line and edit where it says CODE, the code will not run correctly in its current state
     This program will help you practice the main protion of the exam question i.e. writing a function (powerIncrementer) that takes a pointer parameter.
     If you're stuck ask for help early and often :D
 
@@ -47,4 +47,5 @@ void powerIncrementer(int placeholder){ //CODE: Modify the parameter to a pointe
     placeholder = placeholder + 1; // CODE: Would this line change placeholder's value when passed in? Modify it accordingly
 
 }
+
 
